07f693609917af1b42760f595789f99dcd6e563a
Thom <Thombog@gmail.com>
2023-04-22 22:35:19 +0200

    Added fixes

--- hiber.py
+++ hiber.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import subprocess, os, math
-
+ 
 def getSysRam():
     mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
     mem_gib = mem_bytes/(1024.**3)  # e.g. 3.74
-    return
-
+    return mem_gib
+ 
 def run(runCmd):
     output = []
     with subprocess.Popen([runCmd], stdout=subprocess.PIPE, shell=True) as proc:
@@ -17,7 +17,7 @@ def run(runCmd):
             print(read)
             output.extend(read.split('\n'))
     return output
-
+ 
 run('sudo pacman -Syy')
 run('sudo pacman --noconfirm --needed -S linux-zen linux-zen-headers')
 run('sudo pacman --noconfirm -R linux linux-headers')
@@ -25,10 +25,11 @@ run('yay -S --noconfirm --needed update-grub uswsusp')
 run('sudo swapoff -a')
 if(os.path.exists('/swapfile')):
     run('sudo rm /swapfile')
+    pass
 else:
     run('sudo sh -c "echo -e \"/swapfile swap swap defaults 0 0\" >> /etc/fstab"')
-
-run('sudo fallocate -l ' + math.ceil(getSysRam()) + 2 + 'G /swapfile')
+ 
+run('sudo fallocate -l ' + str(math.ceil(getSysRam()) + 2) + 'G /swapfile')
 run('sudo chmod 600 /swapfile')
 run('sudo mkswap /swapfile')
 run('sudo swapon /swapfile')
@@ -39,8 +40,6 @@ uuid = run('findmnt -no UUID -T /swapfile')[0]
 run('sudo sed -i "s/loglevel=3\'/loglevel=3 resume=UUID=' + uuid + ' resume_offset=' + resumeOffset + '\'/g" /etc/default/grub')
 run('sudo update-grub')
 run('sudo mkdir -p /etc/systemd/system/systemd-logind.service.d/')
-run('cat <<-EOF | sudo tee /etc/systemd/system/systemd-logind.service.d/override.conf \
-[Service] \
-Environment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1 \
-EOF')
+run('sudo sh -c "echo -e \"[Service]\" > /etc/systemd/system/systemd-logind.service.d/override.conf"')
+run('sudo sh -c "echo -e \"Environment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1\" >> /etc/systemd/system/systemd-logind.service.d/override.conf"')
 print('Done')
